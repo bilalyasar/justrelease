26f224ebb7fd380ef2d2d849f5b2ccff678372cb
Mesut Celik <mesut@hazelcast.com>
2015-04-25 14:19:19 +0300

    use versioninfo from maven release plugin

--- src/main/java/com/justrelease/Cli.java
+++ src/main/java/com/justrelease/Cli.java
@@ -5,13 +5,9 @@ import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
-import org.apache.commons.io.FileUtils;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.transport.CredentialsProvider;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.apache.maven.shared.release.versions.DefaultVersionInfo;
+import org.apache.maven.shared.release.versions.VersionParseException;
 
-import java.io.File;
-import java.util.Iterator;
 import java.util.logging.Logger;
 
 public class Cli {
@@ -19,7 +15,8 @@ public class Cli {
     private String[] args = null;
     private Options options = new Options();
 
-    public Cli(String[] args) {
+    public Cli(String[] args) throws VersionParseException {
+
 
         this.args = args;
 
@@ -28,8 +25,6 @@ public class Cli {
         options.addOption("name", true, "github username");
         options.addOption("password", true, "github password");
         options.addOption("c", true, "current snapshot version");
-        options.addOption("r", true, "release version");
-        options.addOption("n", true, "next snapshot version");
         options.addOption("h", false, "help");
 
     }
@@ -37,14 +32,12 @@ public class Cli {
     public void parse() {
         CommandLineParser parser = new BasicParser();
 
-        CommandLine cmd = null;
+        CommandLine cmd;
         try {
             cmd = parser.parse(options, args);
             String repo="";
             String localDirectory="";
             String currentVersion="";
-            String releaseVersion="";
-            String nextVersion="";
             String name="";
             String password="";
 
@@ -68,42 +61,42 @@ public class Cli {
                 localDirectory=cmd.getOptionValue("localDirectory");
                 System.out.println("repo url:"+cmd.getOptionValue("localDirectory"));
             }
+            DefaultVersionInfo versionInfo = null;
+
 
             if (cmd.hasOption("c")) {
                 currentVersion=cmd.getOptionValue("c");
-                System.out.println("releasing to the version:"+cmd.getOptionValue("c"));
+                versionInfo = new DefaultVersionInfo(currentVersion);
+                System.out.println("current version:"+versionInfo);
             }
-            if (cmd.hasOption("r")) {
-                releaseVersion=cmd.getOptionValue("r");
-                System.out.println("releasing to the version:"+cmd.getOptionValue("r"));
-            }
+                String releaseVersion=versionInfo.getReleaseVersionString();
+                System.out.println("releasing to the version:"+releaseVersion);
 
-            if (cmd.hasOption("n")) {
-                nextVersion=cmd.getOptionValue("n");
-                System.out.println("updating to the next version:"+cmd.getOptionValue("n"));
-            }
+                String nextVersion=versionInfo.getNextVersion().getSnapshotVersionString();
+                System.out.println("updating to the next version:"+nextVersion);
 
-            FileUtils.deleteDirectory(new File(localDirectory));
 
-            CredentialsProvider cp = new UsernamePasswordCredentialsProvider(name, password);
-            Git.cloneRepository()
-                    .setURI(repo)
-                    .setDirectory(new File(localDirectory))
-                    .setCredentialsProvider(cp)
-                    .call();
-
-            Iterator it = FileUtils.iterateFiles(new File(localDirectory), null, false);
-            while(it.hasNext()){
-                File f = (File)it.next();
-                String content = FileUtils.readFileToString(f);
-                FileUtils.writeStringToFile(f, content.replaceAll(currentVersion, releaseVersion));
-            }
-
-            Git git = Git.open(new File(localDirectory));
-
-            git.add().addFilepattern(".").call();
-            git.commit().setCommitter("justrelease","info@justrelease.com").setMessage(releaseVersion).call();
-            git.push().setCredentialsProvider(cp).call();
+//            FileUtils.deleteDirectory(new File(localDirectory));
+//
+//            CredentialsProvider cp = new UsernamePasswordCredentialsProvider(name, password);
+//            Git.cloneRepository()
+//                    .setURI(repo)
+//                    .setDirectory(new File(localDirectory))
+//                    .setCredentialsProvider(cp)
+//                    .call();
+//
+//            Iterator it = FileUtils.iterateFiles(new File(localDirectory), null, false);
+//            while(it.hasNext()){
+//                File f = (File)it.next();
+//                String content = FileUtils.readFileToString(f);
+//                FileUtils.writeStringToFile(f, content.replaceAll(currentVersion, releaseVersion));
+//            }
+//
+//            Git git = Git.open(new File(localDirectory));
+//
+//            git.add().addFilepattern(".").call();
+//            git.commit().setCommitter("justrelease","info@justrelease.com").setMessage(releaseVersion).call();
+//            git.push().setCredentialsProvider(cp).call();
 
 
         } catch (Exception e) {
--- src/main/java/com/justrelease/JustRelease.java
+++ src/main/java/com/justrelease/JustRelease.java
@@ -1,8 +1,10 @@
 package com.justrelease;
 
+import org.apache.maven.shared.release.versions.VersionParseException;
+
 public class JustRelease {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws VersionParseException {
         new Cli(args).parse();
 
     }
