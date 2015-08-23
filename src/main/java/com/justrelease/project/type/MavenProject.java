5d304614e410f2e7f9ed18fdeb48e70061d91e97
bilalyasar <bilalyasar45@gmail.com>
2015-08-23 23:12:14 +0300

    minor fix

--- src/main/java/com/justrelease/project/type/MavenProject.java
+++ src/main/java/com/justrelease/project/type/MavenProject.java
@@ -2,11 +2,6 @@ package com.justrelease.project.type;
 
 import com.justrelease.config.ReleaseConfig;
 import org.apache.commons.cli.CommandLine;
-import org.apache.maven.model.Model;
-import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-
-import java.io.File;
-import java.io.FileInputStream;
 
 /**
  * Created by bilal on 25/07/15.
@@ -19,30 +14,31 @@ public class MavenProject extends AbstractProjectInfo implements ProjectInfo {
     }
 
     public String getCurrentVersion() {
-        if (!releaseConfig.getCurrentVersion().equals("")) return releaseConfig.getCurrentVersion();
-        MavenXpp3Reader reader = new MavenXpp3Reader();
-        Model result = null;
-        try {
-            String workingDir = System.getProperty("user.dir");
-            result = reader.read(new FileInputStream(workingDir +
-                    File.separator +
-                    releaseConfig.getLocalDirectory() +
-                    File.separator +
-                    releaseConfig.getMainRepo().getDirectory() +
-                    File.separator +
-                    "pom.xml"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        releaseConfig.setCurrentVersion(result.getVersion());
-        return result.getVersion();
+        return "";
+//        if (!releaseConfig.getCurrentVersion().equals("")) return releaseConfig.getCurrentVersion();
+//        MavenXpp3Reader reader = new MavenXpp3Reader();
+//        Model result = null;
+//        try {
+//            String workingDir = System.getProperty("user.dir");
+//            result = reader.read(new FileInputStream(workingDir +
+//                    File.separator +
+//                    releaseConfig.getLocalDirectory() +
+//                    File.separator +
+//                    releaseConfig.getMainRepo().getDirectory() +
+//                    File.separator +
+//                    "pom.xml"));
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        releaseConfig.setCurrentVersion(result.getVersion());
+//        return result.getVersion();
     }
 
     public void createArtifacts() {
-        String workingDir = System.getProperty("user.dir");
-        String[] cmd = {"/bin/sh", "-c", "cd " + workingDir + "/" + releaseConfig.getLocalDirectory() +
-                File.separator +
-                releaseConfig.getMainRepo().getDirectory() +"; mvn clean install -DskipTests"};
-        runCommand(cmd);
+//        String workingDir = System.getProperty("user.dir");
+//        String[] cmd = {"/bin/sh", "-c", "cd " + workingDir + "/" + releaseConfig.getLocalDirectory() +
+//                File.separator +
+//                releaseConfig.getMainRepo().getDirectory() +"; mvn clean install -DskipTests"};
+//        runCommand(cmd);
     }
 }
